11bef93c8d563950d771c954236f41c786d0ee4b
ashwinijawadwar <jawadwarashwini2004@gmail.com>
2025-01-25 14:36:16 +0530

    Update test.sh

--- test/test.sh
+++ test/test.sh
@@ -1,3 +1,5 @@
-#!/bin/bash
-
-curl -I http://localhost
+# Retry loop to wait for Nginx to be available
+until curl -s http://<live-server-ip>:80; do
+  echo "Waiting for Nginx to be available..."
+  sleep 5
+done
